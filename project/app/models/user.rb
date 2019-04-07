cdf094e145861d97cc459a7c3362d52524cb190e
MartinConcha <maconcha1@miuandes.cl>
2019-04-07 18:44:07 -0400

    Asociasiones

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	has_many :comments
+	has_many :posts
+	has_many :likes
+	has_many :dislikes
+	has_many :notifications
+	has_many :inappropriate_content
+	has_one :user_profile
 end
