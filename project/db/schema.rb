ab43d164ea9c46800f31d6e7be6709d49b83f3bb
Josefina <josefina@MacBook-Pro-de-Josefina-2.local>
2019-04-07 14:12:30 -0400

    Second commit for Project

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_175012) do
+ActiveRecord::Schema.define(version: 2019_04_07_180513) do
 
   create_table "administrators", force: :cascade do |t|
     t.string "name"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 2019_04_07_175012) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "logins", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "notifications", force: :cascade do |t|
